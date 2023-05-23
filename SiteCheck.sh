5a7c3b7358fc97195d7d0f9fda0c8cc1976f41a1
Quinn Favo <qfavo@abacusgroupllc.com>
2023-05-23 09:56:33 -0400

    test

--- SiteCheck.sh
+++ SiteCheck.sh
@@ -3,27 +3,27 @@
 statuscode=$(curl --write-out \\n%{http_code} --silent --head https://wmscog.com)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo "Site status changed to $statuscode" | sendmail quazmoz@vivaldi.net
-    else exit 0
-    fi
-
-statuscode=$(curl --write-out \\n%{http_code} --silent --head https://http://goibberifbskjd.com)
-
-    if [[ "$statuscode" -ne 200 ]] ;
     then echo "wmscog.com status changed to $statuscode" | sendmail quazmoz@vivaldi.net
     else exit 0
     fi
 
-statuscode=$(curl --write-out \\n%{http_code} --silent --head https://wmscog.com)
+statuscode=$(curl --write-out \\n%{http_code} --silent --head https://goibberifbskjd.com)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo "Site status changed to $statuscode" | mail -s "wmscog.com may be down, please verify" "quinn.favo@zionusa.org" -r "STATUS_CHECKER"
-    else exit 0
+    then echo "wmscog.com may be down, please verify" | sendmail quazmoz@vivaldi.net
+    else echo "wmscog.com is up"
     fi
 
-statuscode=$(curl --write-out \\n%{http_code} --silent --head https://wmscog.com)
+statuscode=$(curl --write-out \\n%{http_code} --silent --head https://goibberifbskjd.com)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo "Site status changed to $statuscode" | mail -s "wmscog.com may be down, please verify" "quinn.favo@zionusa.org" -r "STATUS_CHECKER"
-    else exit 0
+    then echo "wmscog.com may be down, please verify" | sendmail quazmoz@vivaldi.net
+    else echo "wmscog.com is up"
+    fi
+
+statuscode=$(curl --write-out \\n%{http_code} --silent --head https://goibberifbskjd.com)
+
+    if [[ "$statuscode" -ne 200 ]] ;
+    then echo "wmscog.com may be down, please verify" | sendmail quazmoz@vivaldi.net
+    else echo "wmscog.com is up"
     fi
