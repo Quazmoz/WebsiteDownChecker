3b32ecc9766f4725ee566b95dc30786abeadf8aa
Quinn Favo <qfavo@abacusgroupllc.com>
2023-05-25 16:44:26 -0400

    push change

--- SiteCheck.sh
+++ SiteCheck.sh
@@ -3,48 +3,48 @@
 statuscode=$(curl --write-out \\n%{http_code} --silent --head https://wmsfdgfdgscog.com)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo -e "Subject:wmscog.com may be down\nPlease verify: https://wmscog.com" | sendmail "philip.browning@zionusa.org,gabriel.guzman@zionusa.org,quinn.favo@zionusa.org"
+    then echo -e "Subject:wmscog.com may be down\nPlease verify: https://wmscog.com" | sendmail "qmf1@pitt.edu"
     else echo "wmscog.com is up"
     fi
 
 statuscode=$(curl --write-out \\n%{http_code} --silent --head https://connecticutwmscog.com)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo "https://connecticutwmscog.com may be down, please verify" | sendmail "philip.browning@zionusa.org,gabriel.guzman@zionusa.org,quinn.favo@zionusa.org"
+    then echo "https://connecticutwmscog.com may be down, please verify" | sendmail "qmf1@pitt.edu"
     else echo "https://connecticutwmscog.com is up"
     fi
 
 statuscode=$(curl --write-out \\n%{http_code} --silent --head https://floridawmscog.com)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo "https://floridawmscog.com may be down, please verify" | sendmail "philip.browning@zionusa.org,gabriel.guzman@zionusa.org,quinn.favo@zionusa.org"
+    then echo "https://floridawmscog.com may be down, please verify" | sendmail "qmf1@pitt.edu"
     else echo "https://floridawmscog.com is up"
     fi
 
 statuscode=$(curl --write-out \\n%{http_code} --silent --head https://wmscog.com/wp-admin)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo "https://wmscog.com/wp-admin may be down, please verify" | sendmail "philip.browning@zionusa.org,gabriel.guzman@zionusa.org,quinn.favo@zionusa.org"
+    then echo "https://wmscog.com/wp-admin may be down, please verify" | sendmail "qmf1@pitt.edu"
     else echo "https://wmscog.com/wp-admin is up"
     fi
 
 statuscode=$(curl --write-out \\n%{http_code} --silent --head https://newyorkwmscog.com/wp-admin)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo "https://newyorkwmscog.com/wp-admin may be down, please verify" | sendmail "philip.browning@zionusa.org,gabriel.guzman@zionusa.org,quinn.favo@zionusa.org"
+    then echo "https://newyorkwmscog.com/wp-admin may be down, please verify" | sendmail "qmf1@pitt.edu"
     else echo "https://newyorkwmscog.com/wp-admin is up"
     fi
 
 statuscode=$(curl --write-out \\n%{http_code} --silent --head https://floridawmscog.com/wp-admin)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo "https://floridawmscog.com/wp-admin may be down, please verify" | sendmail "philip.browning@zionusa.org,gabriel.guzman@zionusa.org,quinn.favo@zionusa.org"
+    then echo "https://floridawmscog.com/wp-admin may be down, please verify" | sendmail "qmf1@pitt.edu"
     else echo "https://floridawmscog.com/wp-admin is up"
     fi
 
 statuscode=$(curl --write-out \\n%{http_code} --silent --head https://connecticutwmscog.com/wp-admin)
 
     if [[ "$statuscode" -ne 200 ]] ;
-    then echo "https://connecticutwmscog.com/wp-admin may be down, please verify" | sendmail "philip.browning@zionusa.org,gabriel.guzman@zionusa.org,quinn.favo@zionusa.org"
+    then echo "https://connecticutwmscog.com/wp-admin may be down, please verify" | sendmail "qmf1@pitt.edu"
     else echo "https://connecticutwmscog.com/wp-admin is up"
     fi
